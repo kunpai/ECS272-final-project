bed4b1144f6b72fbcd3f935de4b9ceed0e7bd15b
kunpai <kunpai@ucdavis.edu>
2023-11-30 15:15:22 -0800

    Add Breadcrumb component to PCA Scatterplot

--- ecs-272-fin/src/components/PCA-Scatterplot.js
+++ ecs-272-fin/src/components/PCA-Scatterplot.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { readCSV } from '@/components/util/CSV-Reader';
-import { Button } from "react-bootstrap";
+import { Button, Breadcrumb } from "react-bootstrap";
 
 import ScatterPlot from './graphs/Scatterplot';
 import Tooltip from './graphs/PrincipalComponentPlot';
@@ -14,7 +14,7 @@ export default function PCA_Scatterplot() {
     const [pc2, setPC2] = useState([]);
     const [tooltipAxis, setTooltipAxis] = useState(null);
     const [parameter, setParameter] = useState(null);
-    const [view, setView] = useState('overview');
+    const [view, setView] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -37,22 +37,46 @@ export default function PCA_Scatterplot() {
         window.location.reload(true);
     };
 
+    const viewBreadcrumb = view && view.split('-').map((breadcrumb, index, array) => {
+        const capitalizedBreadcrumb = breadcrumb.charAt(0).toUpperCase() + breadcrumb.slice(1);
+        return (
+            <Breadcrumb.Item
+                key={breadcrumb}
+                onClick={() => setView(array.slice(0, index + 1).join('-'))}
+                active={index === array.length - 1}
+            >
+                {capitalizedBreadcrumb}
+            </Breadcrumb.Item>
+        );
+    });
+
     return (
         <>
-            <div>
-                <Button variant="primary" onClick={handleReload} style={{ marginLeft: '20px', marginTop: '20px' }}>Reload</Button>
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginLeft: '20px', marginTop: '20px' }}>
+                <div>
+                    <Button variant="primary" onClick={handleReload}>Reload</Button>
+                </div>
+                <div style={{ textAlign: 'center', flex: 1 }}>
+                    <Breadcrumb style={{ marginBottom: '10px', display: 'inline-block' }}>
+                        <Breadcrumb.Item onClick={() => setView(null)} active={!view}>PCA Scatter Plot</Breadcrumb.Item>
+                        {viewBreadcrumb}
+                    </Breadcrumb>
+                </div>
             </div>
-            <div style={{ display: 'flex', flexDirection: 'row', paddingLeft: '20px' }}>
-                <ScatterPlot data={data} callbackPC={setTooltipAxis} setParameter={setParameter} setView={setView} />
-                {parameter && view && view.split("-").length <= 3 && (
-                    <StarChart parameter={parameter} view={view} setView={setView} />
-                )}
-                {parameter && view && view.split("-").length > 3 && (
-                    <ParameterBarChart parameter={parameter} view={view} setView={setView}/>
-                )}
-                {tooltipAxis && (
-                    <Tooltip axis={tooltipAxis} data={tooltipAxis === 'PC1' ? pc1 : pc2} />
-                )}
+
+            <div>
+                <div style={{ display: 'flex', flexDirection: 'row', paddingLeft: '20px', justifyContent: 'flex-start' }}>
+                    <ScatterPlot data={data} callbackPC={setTooltipAxis} setParameter={setParameter} setView={setView} />
+                    {parameter && view && view.split("-").length <= 3 && (
+                        <StarChart parameter={parameter} view={view} setView={setView} />
+                    )}
+                    {parameter && view && view.split("-").length > 3 && (
+                        <ParameterBarChart parameter={parameter} view={view} setView={setView}/>
+                    )}
+                    {tooltipAxis && (
+                        <Tooltip axis={tooltipAxis} data={tooltipAxis === 'PC1' ? pc1 : pc2} />
+                    )}
+                </div>
             </div>
         </>
     );
